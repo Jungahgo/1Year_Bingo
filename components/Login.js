4090f0fd75d65324ff45f471220df1dccac30931
곽정아 <goak@Goak-MAC.local>
2023-01-20 22:25:48 +0900

    email form

--- components/Login.js
+++ components/Login.js
@@ -1,8 +1,45 @@
+import {useState} from "react";
+
 export default function Login(){
+    const [email, setEmail] = useState('');
+
+    const handleChange = (e) => {
+        setEmail(e.target.value);
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        alert("submit", email);
+    }
+
+    function sendMail(e){
+
+    }
+
+    function getCertificationNum(){
+        alert("email: ", email);
+    }
     return (
-        <div>
-            <h1>Put your Email</h1>
-            <input type="Email" placeholder="email"/>
-        </div>
+        <>
+          <h1>Put your Email</h1>
+          <form onSubmit={handleSubmit}>
+                <input 
+                    type="Email" 
+                    name="email"
+                    placeholder="email"
+                    value = {email}
+                    onChange={handleChange}
+                />
+                <button type="submit" onClick={sendMail}>인증번호 전송</button>
+          </form>
+          <b>{email}</b>
+          <form>
+                <input 
+                    type="Text" 
+                    placeholder="인증번호"
+                />
+                <button type="submit" onClick={getCertificationNum}>확인</button>
+          </form>
+        </>
     )
 }
